41093d58fb5e0a7c2bd95704f22b348cfaa7bc6d
aprilnovak <novak@berkeley.edu>
2017-04-05 13:42:01 -0700

    initialized problem variables.

--- main.f90
+++ main.f90
@@ -13,6 +13,8 @@ integer             :: AllocateStatus ! variable to hold memory allocation succe
 ! program variables related to FEM solve
 real(rk) :: length             ! length of the domain (1-D)
 integer  :: n_el               ! number of elements
+integer  :: n_en               ! number of nodes per element
+integer  :: n_nodes            ! total number of nodes
 integer  :: order              ! polynomial order
 real (rk), dimension(:), allocatable :: x  ! coordinates of the nodes
 real(rk) :: h                  ! length of one element
@@ -21,18 +23,16 @@ real (rk), dimension(:, :), allocatable :: qp ! quadrature points and weights
 ! parse command line arguments
 call commandline(length, n_el, order)
 
-h = length / real(n_el)
+! initialize problem variables
+call initialize(h, x, n_en, n_el, order, n_nodes)
 
-! allocate memory for the vector of node coordinates
-allocate(x(n_el + 1), stat = AllocateStatus)
-if (AllocateStatus /= 0) STOP "Allocation of x array failed."
+
+
+
 
 allocate(qp(ceiling((real(order) + 1.0) / 2.0), 2), stat = AllocateStatus)
 if (AllocateStatus /= 0) STOP "Allocation of qp array failed."
 
-do i = 1, size(x)
-  x(i) = 1.0
-end do
 
 ! initialize the quadrature rule
 call quadrature(order)
@@ -50,8 +50,6 @@ CONTAINS
 
 subroutine quadrature(order)
   integer, intent(in) :: order
-
-  write(*,*) "quadrature subroutine."
 end subroutine quadrature
 
 
@@ -85,6 +83,26 @@ subroutine commandline(length, n_el, order)
     end select  
   enddo
 end subroutine commandline
+
+
+subroutine initialize(h, x, n_en, n_el, order, n_nodes)
+  real(rk), intent(out) :: h ! element length
+  real (rk), dimension(:), allocatable, intent(out) :: x ! node coordinates
+  integer, intent(out) :: n_en    ! number of nodes per element
+  integer, intent(in)  :: n_el    ! number of elements
+  integer, intent(in)  :: order   ! polynomial order
+  integer, intent(out) :: n_nodes ! number of total nodes
+
+  h = length / real(n_el)
+  n_en = order + 1
+  n_nodes = (order + 1) * n_el - (n_el - 1)
+   
+  ! allocate memory for the vector of node coordinates
+  allocate(x(n_nodes), stat = AllocateStatus)
+  if (AllocateStatus /= 0) STOP "Allocation of x array failed."
+
+end subroutine initialize
+
 
 
 END PROGRAM main
