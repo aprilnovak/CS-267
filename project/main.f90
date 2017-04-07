8bbf4274192bdc58dc87d7464569087e79d28311
aprilnovak <novak@berkeley.edu>
2017-04-06 21:59:34 -0700

    works for two elements only.

--- project/main.f90
+++ project/main.f90
@@ -1,7 +1,15 @@
 ! program to solve the heat equation (Dirichlet boundary conditions only)
+
+! fortran arrays are read and written in column order
+! fortran arrays are only stored this way - the first index stil refers to the row 
+! and the second to the column?
+
+! however, you still use the same indices to refer to the elements of a matrix
+
+! first row: kel(1, :)
+
 PROGRAM main
 
-! turn off implicit typing
 implicit none
 
 ! define double precision with 15 digits of accuracy
@@ -26,7 +34,8 @@ real(rk) :: convergence        ! difference between successive iterations
 real(rk) :: tol                ! conjugate gradient convergence tolerance
 
 integer,  dimension(:, :), allocatable :: LM     ! location matrix
-real(rk), dimension(:, :), allocatable :: qp     ! quadrature points and weights
+real(rk), dimension(:),    allocatable :: qp     ! quadrature points
+real(rk), dimension(:),    allocatable :: wt     ! quadrature weights
 real(rk), dimension(:),    allocatable :: x      ! coordinates of the nodes
 real(rk), dimension(:, :), allocatable :: kel    ! elemental stiffness matrix
 real(rk), dimension(:),    allocatable :: rel    ! elemental load vector
@@ -47,7 +56,7 @@ tol = 0.001
 
 call commandline(length, n_el, order)              ! parse command line arguments
 call initialize(h, x, n_en, n_el, order, n_nodes)  ! initialize problem variables
-call quadrature(order, n_qp, qp)                   ! initialize quadrature rule
+call quadrature(order, n_qp, qp, wt)               ! initialize quadrature rule
 call phi_val(order, qp, phi, dphi)                 ! initialize shape functions
 
 ! form the elemental stiffness matrix and load vector
@@ -58,15 +67,17 @@ if (AllocateStatus /= 0) STOP "Allocation of rel array failed."
 
 kel = 0.0
 rel = 0.0
-! only works for 1-D elements
 do q = 1, n_qp
   do i = 1, n_en
-    rel(i) = rel(i) + qp(q, 2) * source * phi(q, i) * h / 2.0
+    rel(i) = rel(i) + wt(q) * source * phi(i, q) * h / 2.0
     do j = 1, n_en
-      kel(i, j) = kel(i, j) + qp(q, 2) * dphi(q, i) * k * dphi(q, j) * 2.0 / h
+      kel(i, j) = kel(i, j) + wt(q) * dphi(i, q) * k * dphi(j, q) * 2.0 / h
     end do
   end do
 end do
+
+print *, 'elementary stiffness matrix: ', kel
+print *, 'elementary load vector: ', rel
 
 ! form the location matrix
 call locationmatrix(LM, n_el, n_en)
@@ -88,7 +99,6 @@ do q = 1, n_el
   end do
 end do
 
-
 allocate(a(n_nodes), stat = AllocateStatus)
 if (AllocateStatus /= 0) STOP "Allocation of a array failed."
 allocate(aprev(n_nodes), stat = AllocateStatus)
@@ -97,57 +107,66 @@ allocate(z(n_nodes), stat = AllocateStatus)
 if (AllocateStatus /= 0) STOP "Allocation of z array failed."
 allocate(zprev(n_nodes), stat = AllocateStatus)
 if (AllocateStatus /= 0) STOP "Allocation of zprev array failed."
-
 allocate(res(n_nodes), stat = AllocateStatus)
 if (AllocateStatus /= 0) STOP "Allocation of res array failed."
 
-
+! apply boundary conditions
 kglob(1, :) = 0.0
-kglob(1, 1) = 1.0                ! set up left BC              
-
 kglob(n_nodes, :) = 0.0
+kglob(1, 1) = 1.0                ! set up left BC              
 kglob(n_nodes, n_nodes) = 1.0    ! set up right BC
-
 rglob(1) = 2.0                   ! left BC value
 rglob(n_nodes) = 1.0             ! right BC value     
 
 
-!print *, 'solving matrix system: kglob: ', kglob(:, :)
+print *, 'solving matrix system: kglob: '
+print *, kglob(1, :)
+print *, kglob(2, :)
+print *, kglob(3, :)
 !print *, 'with load vector: ', rglob(:)
 
 ! conjugate gradient solver for solving kglob * a = rglob
 aprev  = 0.0
-print *, 'first guess: ', aprev(:)
 res    = rglob - matmul(kglob, aprev)
-print *, 'matrix-vector product: ', matmul(kglob, aprev)
 zprev  = res
 
 ! if we hit it on the head, then don't do any more iterations
 ! check to make sure the residual is not already zero
-
-
+if (dot_product(res, res) <= tol) then
+  print *, 'residual is essentially zero.'
+end if
 
 lambda = dot_product(zprev, res) / dot_product(zprev, matmul(kglob, zprev))
 a      = aprev + lambda * zprev
 print *, 'first residual: ', res, '\n'
-print *, 'lambda: ', lambda
-print *, 'a: ', a
+!print *, 'lambda: ', lambda
+print *, 'first update: ', a
 
 convergence = 0.0
 do i = 1, n_nodes
   convergence = convergence + abs(a(i) - aprev(i))
 end do
 
+print *, convergence
+
+j = 1
 do while (convergence > tol)
   aprev  = a
   res    = rglob - matmul(kglob, a)
+  print *, 'next residual: ', res
+
   theta  = - dot_product(res, matmul(kglob, zprev)) / dot_product(zprev, matmul(kglob, zprev))
+
+  !if (theta <= epsilon(1.0)) then
+  !  exit
+  !end if
+
   z      = res + theta * zprev
   lambda = dot_product(z, res) / dot_product(z, matmul(kglob, z))
   a      = a + lambda * z
   zprev  = z
   
-  print *, 'residual: ', res(:)
+  !print *, 'residual: ', res(:)
   !print *, 'theta: ', theta
   !print *, 'z: ', z(:)
   !print *, 'lambda: ', lambda
@@ -159,6 +178,7 @@ do while (convergence > tol)
   end do
   
   print *, 'error: ', convergence
+  j = j + 1
 end do
 
 ! write to an output file for plotting. If this file exists, it will be re-written.
@@ -184,7 +204,7 @@ write(1, *) a(:)
 !end do
 
 ! deallocate memory 
-deallocate(qp, x, kel, rel, phi, dphi, kglob, rglob, a, aprev, z, zprev, res)
+deallocate(qp, wt, x, kel, rel, phi, dphi, kglob, rglob, a, aprev, z, zprev, res)
 
 
 
@@ -202,7 +222,7 @@ subroutine locationmatrix(LM, n_el, n_en)
   
   do i = 1, n_el
     do j = 1, n_en
-LM(i, j) = 1 + (i - 1) * (n_en - 1) + (j - 1)
+      LM(i, j) = 1 + (i - 1) * (n_en - 1) + (j - 1)
     end do
   end do
 end subroutine locationmatrix
@@ -210,62 +230,65 @@ end subroutine locationmatrix
 subroutine phi_val(order, qp, phi, dphi)
   implicit none
   integer, intent(in)  :: order
-  real(rk), dimension(:, :), intent(in)  :: qp
+  real(rk), dimension(:), intent(in)  :: qp
   real(rk), dimension(:, :), allocatable, intent(inout) :: phi
   real(rk), dimension(:, :), allocatable, intent(inout) :: dphi
 
   ! allocate memory for the shape functions - quadrature points
   ! do not change throughout the simulation
-  allocate(phi(n_qp, order + 1), stat = AllocateStatus)
+  allocate(phi(order + 1, n_qp), stat = AllocateStatus)
   if (AllocateStatus /= 0) STOP "Allocation of phi array failed."
 
   ! allocate memory for the shape functions - quadrature points
   ! do not change throughout the simulation
-  allocate(dphi(n_qp, order + 1), stat = AllocateStatus)
+  allocate(dphi(order + 1, n_qp), stat = AllocateStatus)
   if (AllocateStatus /= 0) STOP "Allocation of dphi array failed."
 
   select case(order)
     case(1)
-      phi(:, 1) = (1.0 - qp(:, 1)) / 2.0
-      phi(:, 2) = (1.0 + qp(:, 1)) / 2.0
-      dphi(:, 1) = -1.0 / 2.0
-      dphi(:, 2) =  1.0 / 2.0
+      phi(1, :)  = (1.0 - qp(:)) / 2.0
+      phi(2, :)  = (1.0 + qp(:)) / 2.0
+      dphi(1, :) = -1.0 / 2.0
+      dphi(2, :) =  1.0 / 2.0
     case(2)
-      phi(:, 1) = qp(:, 1) * (qp(:, 1) - 1.0) / 2.0
-      phi(:, 2) = (1.0 - qp(:, 1)) * (1.0 + qp(:, 1))
-      phi(:, 3) = qp(:, 1) * (qp(:, 1) + 1.0) / 2.0
-      dphi(:, 1) = (2.0 * qp(:, 1) - 1.0) / 2.0
-      dphi(:, 2) = 1.0 - qp(:, 1) * qp(:, 1)
-      dphi(:, 3) = (2.0 * qp(:, 1) + 1.0) / 2.0
+      phi(1, :)  = qp(:) * (qp(:) - 1.0) / 2.0
+      phi(2, :)  = (1.0 - qp(:)) * (1.0 + qp(:))
+      phi(3, :)  = qp(1) * (qp(:) + 1.0) / 2.0
+      dphi(1, :) = (2.0 * qp(:) - 1.0) / 2.0
+      dphi(2, :) = 1.0 - qp(:) * qp(:)
+      dphi(3, :) = (2.0 * qp(:) + 1.0) / 2.0
     case default
       write(*,*) "phi and dphi not initialized due to polynomial order not being supported."
   end select
 end subroutine phi_val
 
 
-subroutine quadrature(order, n_qp, qp)
+subroutine quadrature(order, n_qp, qp, wt)
   implicit none
 
   integer, intent(in)  :: order
   integer, intent(out) :: n_qp
-  real (rk), dimension(:, :), allocatable :: qp
+  real (rk), dimension(:), allocatable :: qp
+  real (rk), dimension(:), allocatable :: wt
   
   n_qp = 2
   !n_qp = ceiling((real(order) + 1.0) / 2.0)
 
-  allocate(qp(n_qp, 2), stat = AllocateStatus)
+  allocate(qp(n_qp), stat = AllocateStatus)
   if (AllocateStatus /= 0) STOP "Allocation of qp array failed."
+  allocate(wt(n_qp), stat = AllocateStatus)
+  if (AllocateStatus /= 0) STOP "Allocation of wt array failed."
   
   select case(n_qp)
     case(1)
-      qp(:, 1) = (/ 0.0 /)
-      qp(:, 2) = (/ 2.0 /)
+      qp = (/ 0.0 /)
+      wt = (/ 2.0 /)
     case(2)
-      qp(:, 1) = (/ -1.0/sqrt(3.0), 1.0/sqrt(3.0) /)
-      qp(:, 2) = (/ 1.0, 1.0 /)
+      qp = (/ -1.0/sqrt(3.0), 1.0/sqrt(3.0) /)
+      wt = (/ 1.0, 1.0 /)
     case(3)
-      qp(:, 1) = (/ -sqrt(3.0/5.0), 0.0, sqrt(3.0/5.0) /)
-      qp(:, 2) = (/ 5.0/9.0, 8.0/9.0, 5.0/9.0 /)
+      qp = (/ -sqrt(3.0/5.0), 0.0, sqrt(3.0/5.0) /)
+      wt = (/ 5.0/9.0, 8.0/9.0, 5.0/9.0 /)
     case default
       write(*,*) "Error in selecting quadrature rule."
   end select
@@ -274,7 +297,6 @@ end subroutine quadrature
 
 subroutine commandline(length, n_el, order)
   implicit none
-  ! define subroutine parameters
   real(rk), intent(out) :: length
   integer, intent(out)  :: n_el
   integer, intent(out)  :: order
