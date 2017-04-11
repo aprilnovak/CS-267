cf60dce25b806f3ba29f4b97abd362ae79ae87b1
aprilnovak <novak@berkeley.edu>
2017-04-10 21:19:04 -0700

    added script for plotting the solution.

---
+++ project/solution.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+raw = []
+with open('output.txt', 'r') as f:
+	for line in f:
+		raw = line.split(' ')
+
+data = [raw[i] for i in range(0, len(raw) - 1) if raw[i] != '']
+data = [float(data[i]) for i in range(0, len(data))]
+
+x = np.linspace(0.0, 1.0, len(data))
+plt.plot(x, data, '*-')
+plt.show()
+
+
+# plot the timing data
