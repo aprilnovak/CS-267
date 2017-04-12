3a6822f9c59326b4467827c9b09049eafb6f4f9b
aprilnovak <novak@berkeley.edu>
2017-04-12 16:48:40 -0700

    updated solution plotting script to show the iteration solution evolution.

--- project/solution.py
+++ project/solution.py
@@ -1,17 +1,21 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+nodes = 21
+
 raw = []
+intermediate = []
+data = []
 with open('output.txt', 'r') as f:
 	for line in f:
 		raw = line.split(' ')
+		intermediate = [raw[i] for i in range(0, len(raw) - 1) if raw[i] != '']
+		data.extend(intermediate)
 
-data = [raw[i] for i in range(0, len(raw) - 1) if raw[i] != '']
 data = [float(data[i]) for i in range(0, len(data))]
+x    = np.linspace(0.0, 1.0, len(data[0:nodes]))
 
-x = np.linspace(0.0, 1.0, len(data))
-plt.plot(x, data, '*-')
+for i in range(0, len(data), nodes):
+	plt.plot(x, data[i:(i + nodes)], '*-')
+
 plt.show()
-
-
-# plot the timing data
