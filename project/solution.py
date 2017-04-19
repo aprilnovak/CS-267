2e37559163dc3154b32eb663ef120cb28577c707
aprilnovak <novak@berkeley.edu>
2017-04-19 13:19:25 -0700

    plotting function now obtains number of elements from output file.

--- project/solution.py
+++ project/solution.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-nodes = 21
+#nodes = 101
+it = 1
 
 raw = []
 intermediate = []
@@ -10,12 +11,20 @@ with open('output.txt', 'r') as f:
 	for line in f:
 		raw = line.split(' ')
 		intermediate = [raw[i] for i in range(0, len(raw) - 1) if raw[i] != '']
-		data.extend(intermediate)
+		if (len(intermediate) == 4):
+			numprocs = int(intermediate[0])
+			nodes = int(intermediate[1]) + 1
+			data.extend(intermediate[2:])
+		else:
+			data.extend(intermediate)
 
 data = [float(data[i]) for i in range(0, len(data))]
 x    = np.linspace(0.0, 1.0, len(data[0:nodes]))
 
-for i in range(0, len(data), nodes):
-	plt.plot(x, data[i:(i + nodes)], '*-')
+#plt.ion()
+for i in range(0, len(data), it*nodes):
+	plt.plot(x, data[i:(i + nodes)], '--')
+	#plt.pause(0.1)
 
+#plt.plot(x, data[-nodes:])
 plt.show()
