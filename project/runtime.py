7922841da277a83c4c2bc1bb9fe1e71ebbe990ce
aprilnovak <novak@berkeley.edu>
2017-04-30 22:33:36 -0700

    added figures and plotting scripts.

--- project/runtime.py
+++ project/runtime.py
@@ -13,6 +13,7 @@ serial = []
 parallel = []
 elems = []
 procs = []
+speedup = []
 with open(filename, 'r') as f:
 	for line in f:
 		raw = line.split(' ')
@@ -28,6 +29,7 @@ for i in range(len(data)):
 		procs.append(mpi[i])
 	else:
 		serial.append(data[i])
+		speedup.append(float(data[i])/float(data[i - 1]))
 
 
 data = [float(data[i]) for i in range(0, len(data))]
@@ -35,7 +37,24 @@ mpi = [float(mpi[i]) for i in range(0, len(mpi))]
 nel = [float(nel[i]) for i in range(0, len(nel))]
 #x    = np.linspace(0.0, 1.0, len(data[0:nodes]))
 
-plt.plot(procs, parallel, label='parallel')
-plt.plot(procs, serial, label='serial')
-plt.show()
-#plt.savefig('interface1.png')
+#plt.plot(procs, parallel, 'o-', label='parallel')
+#plt.plot(procs, serial, 'o-', label='serial')
+#plt.plot(procs, speedup, label='speedup')
+#plt.xlabel('Number of MPI Processes')
+#plt.ylabel('Runtime (s)')
+#plt.ylabel('Speedup')
+
+# two axes
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(mpi, parallel, label='parallel')
+ax1.set_ylabel('Parallel Runtime (s)')
+
+ax2 = ax1.twinx()
+ax2.plot(mpi, serial, 'r-')
+ax2.set_ylabel('Serial Runtime (s)', color='r', label='serial')
+for tl in ax2.get_yticklabels():
+    tl.set_color('r')
+plt.legend()
+#plt.show()
+plt.savefig('runtime-multi-node.png')
