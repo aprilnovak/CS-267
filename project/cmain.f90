ee7ed130b18f273fc425af4611a41a95053c8056
April Novak <novak@berkeley.edu>
2017-05-02 11:46:53 -0700

    added basic Makefile.

--- project/cmain.f90
+++ project/cmain.f90
@@ -31,7 +31,6 @@ real(8)  :: startCSR           ! start CSR time
 real(8)  :: endCSR             ! end CSR time
 
 ! variables to define the global problem
-!real(8), dimension(:),    allocatable :: soln        ! global soln vector
 integer                               :: numprocs    ! number of processors
 integer                               :: rank        ! processor rank
 integer                               :: ddcnt       ! DD counter
