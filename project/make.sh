8b6a6f6cc6357ec0f8cc17731eea83d6c7d8fedb
April Novak <novak@berkeley.edu>
2017-05-04 09:24:22 -0700

    deleted old make script because I have a makefile now.

--- project/make.sh
+++
@@ -1,4 +0,0 @@
-# hacky Makefile for OpenMP version
-
-ftn -c -O3 -qopenmp solvers.f90 element_matrices.f90 csr_storage.f90 mesh.f90 quadrature.f90 read_data.f90
-ftn *.o -O3 -qopenmp fmain.f90 -o openmp.out
